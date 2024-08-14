16fc40892668f0124b3a32735bd56ad323f1e3ca
Sean Kahler <itsbattledash@gmail.com>
2024-08-14 04:38:37 -0700

    Test

--- maxima-lib/src/unix/wine.rs
+++ maxima-lib/src/unix/wine.rs
@@ -218,6 +218,10 @@ pub async fn run_wine_command<I: IntoIterator<Item = T>, T: AsRef<OsStr>>(
         .env("LD_PRELOAD", "") // Fixes some log errors for some games
         .arg(arg);
 
+    if wine_path != "umu-run" {
+        child = child.env("WINEDLLOVERRIDES", "CryptBase,bcrypt,dxgi,d3d11,d3d12,d3d12core=n,b;winemenubuilder.exe=d");
+    }
+
     if let Some(arguments) = args {
         child = child.args(arguments);
     }
